90eaf0cdd6a80f0e12aedd6a484ca9b87f2a3536
Laxminayanan <laxminayanan546@gmail.com>
2026-03-31 14:12:02 +0530

    Added `script.js` In The `static`.

--- static/script.js
+++ static/script.js
@@ -1,1 +1,23 @@
+const form = document.getElementById("factorialForm");
+const loading = document.getElementById("loading");
+const input = document.getElementById("numberInput");
 
+form.addEventListener("submit", function () {
+    loading.classList.remove("hidden");
+});
+
+/* CLIENT SIDE VALIDATION */
+input.addEventListener("input", function () {
+    if (isNaN(input.value)) {
+        input.style.border = "2px solid red";
+    } else {
+        input.style.border = "none";
+    }
+});
+
+/* COPY FUNCTION */
+function copyResult() {
+    const text = document.querySelector(".result").innerText;
+    navigator.clipboard.writeText(text);
+    alert("Copied to clipboard!");
+}
